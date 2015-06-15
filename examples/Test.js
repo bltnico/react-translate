146a2efb0381781192b4d5979f6a6b48079b7680
bltnico <bellot.n@gmail.com>
2015-06-15 14:50:46 +0200

    Update example

--- examples/Test.js
+++ examples/Test.js
@@ -16,39 +16,39 @@ Translate.registerTranslation("en_US", {
 
 Translate.autoDetectLocale(true);
 Translate.setAvailableLocales(['fr_FR','en_US']);
-Translate.setLocale("fr_FR");
+Translate.initTranslation("fr_FR");
 
-var LangSwitcher = React.createClass({
+var LangSwitcher = React.createClass({displayName: "LangSwitcher",
     handleChange : function(e) {
         Translate.updateTranslation(e.target.value);
     },
     render : function() {
         return (
-            <select onChange={this.handleChange}>
-                <option value="fr_FR">Français</option>
-                <option value="en_US">Anglais</option>
-            </select>
+            React.createElement("select", {onChange: this.handleChange},
+                React.createElement("option", {value: "fr_FR"}, "Français"),
+                React.createElement("option", {value: "en_US"}, "Anglais")
+            )
         )
     }
 });
 
-var Test = React.createClass({
+var Test = React.createClass({displayName: "Test",
     render : function() {
         return (
-            <div>
-                <Translate from="REACT" element="h1"/>
-                <p><Translate from="TEST" /></p>
-                <div>
-                    <Translate from="TITLE" element="b" />
-                    <div>
-                        <LangSwitcher />
-                    </div>
-                    <button><Translate from="START_WITH" /></button>
-                    <Translate from="START_WITH" element="input" />
-                </div>
-            </div>
+            React.createElement("div", null,
+                React.createElement(Translate, {from: "REACT", element: "h1"}),
+                React.createElement("p", null, React.createElement(Translate, {from: "TEST"})),
+                React.createElement("div", null,
+                    React.createElement(Translate, {from: "TITLE", element: "b"}),
+                    React.createElement("div", null,
+                        React.createElement(LangSwitcher, null)
+                    ),
+                    React.createElement("button", null, React.createElement(Translate, {from: "START_WITH"})),
+                    React.createElement(Translate, {from: "START_WITH", element: "input"})
+                )
+            )
         )
     }
 });
 
-React.render(<Test />, document.body);
+React.render(React.createElement(Test, null), document.body);
