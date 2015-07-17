984cb214faaa43f211946ea19920efb0eb5c06a8
bltnico <bellot.n@gmail.com>
2015-07-17 11:00:12 +0200

    Start doc

---
+++ scripts/Menu.jsx
@@ -0,0 +1,49 @@
+'use strict'
+
+var MenuList = {
+    "Components" : [
+        "Basic",
+        "With defaut lang",
+        "With DOM element",
+        "With custom React component",
+        "With props"
+    ],
+    "Methods" : [
+        "bla",
+        "bla",
+        "bla",
+        "bla",
+        "bla"
+    ]
+};
+
+var Menu = React.createClass({
+
+    render : function() {
+        return (
+            <div>
+                <ul>
+                    {MenuList.Components.map(function(m,i){
+                        return (
+                            <li>
+                                <a href={"#" + (m.replace(/\s/g,"-")).toLowerCase()} target="_self">{m}</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <ul>
+                    {MenuList.Methods.map(function(m,i){
+                        return (
+                            <li>
+                                <a href={"#" + (m.replace(/\s/g,"-")).toLowerCase()} target="_self">{m}</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
+
+});
+
+React.render(<Menu />, document.getElementById("menu-react-render"));
